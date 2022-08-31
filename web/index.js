cbe2ac8989395db88c998013ac296101e7fe5c0b
justken <u0024window@gmail.com>
2022-08-30 17:55:20 -0700

    up

--- web/index.js
+++ web/index.js
@@ -1,39 +1,47 @@
-async function handleFilesUpload(e) {
-  const file = e.target.files[0];
-  const data = await file.arrayBuffer();
-  const workbook = XLSX.read(data);
-  const excelData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
-  console.log(excelData)
-}
-$(document).ready(function () {
-  $('#excelTable').DataTable({
+$('#input-excel').change(function (e) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.readAsArrayBuffer(file);
+  reader.onload = (e) => {
+
+    // upload file
+    const binarystr = new Uint8Array(e.target.result);
+    const wb = XLSX.read(binarystr, { type: 'array', raw: true, cellFormula: false });
+    console.log(wb.Sheets)
+
+    const wsname = wb.SheetNames[0];
+    const data = XLSX.utils.sheet_to_json(wb.Sheets[wsname]);
+    console.log(data)
+  }
+})
+$('#excelTable').DataTable({
     paging: false,
     searching: false,
     data: [
       {
-          "name":       "Tiger Nixon",
-          "position":   "System Architect",
-          "salary":     "$3,120",
-          "start_date": "2011/04/25",
-          "office":     "Edinburgh",
-          "extn":       "5421"
+        "name": "Tiger Nixon",
+        "position": "System Architect",
+        "salary": "$3,120",
+        "start_date": "2011/04/25",
+        "office": "Edinburgh",
+        "extn": "5421"
       },
       {
-          "name":       "Garrett Winters",
-          "position":   "Director",
-          "salary":     "$5,300",
-          "start_date": "2011/07/25",
-          "office":     "Edinburgh",
-          "extn":       "8422"
+        "name": "Garrett Winters",
+        "position": "Director",
+        "salary": "$5,300",
+        "start_date": "2011/07/25",
+        "office": "Edinburgh",
+        "extn": "8422"
       }
-  ],
+    ],
     columns: [
-      { data: 'name' },
-      { data: 'salary' },
-      { data: 'office' },
-      { data: 'position' },
-      { data: 'start_date' },
-      { data: 'extn' }
+      { title: 'name', data: 'name' },
+      { title: 'salary', data: 'salary' },
+      { title: 'office', data: 'office' },
+      { title: 'position', data: 'position' },
+      { title: 'start_date', data: 'start_date' },
+      { title: 'extn', data: 'extn' }
     ]
   });
-});
