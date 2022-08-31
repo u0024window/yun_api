f68840fdb2547e6dba0ed4fb873a15470498bb0d
justken <u0024window@gmail.com>
2022-08-30 17:32:50 -0700

    up

--- web/index.js
+++ web/index.js
@@ -1,21 +1,39 @@
-import * as XLSX from 'xlsx/xlsx.mjs';
-import Table from 'rc-table';
-import React from 'react';
-
-function handleFilesUpload(e) {
+async function handleFilesUpload(e) {
   const file = e.target.files[0];
   const data = await file.arrayBuffer();
   const workbook = XLSX.read(data);
-  const excelData=XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+  const excelData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
   console.log(excelData)
-  }
-
-
-  function excelTotable(){
-    var columns=[]
-    var data=[]
-  }
-
-  const container = document.getElementById('app');
-  
-  
+}
+$(document).ready(function () {
+  $('#excelTable').DataTable({
+    paging: false,
+    searching: false,
+    data: [
+      {
+          "name":       "Tiger Nixon",
+          "position":   "System Architect",
+          "salary":     "$3,120",
+          "start_date": "2011/04/25",
+          "office":     "Edinburgh",
+          "extn":       "5421"
+      },
+      {
+          "name":       "Garrett Winters",
+          "position":   "Director",
+          "salary":     "$5,300",
+          "start_date": "2011/07/25",
+          "office":     "Edinburgh",
+          "extn":       "8422"
+      }
+  ],
+    columns: [
+      { data: 'name' },
+      { data: 'salary' },
+      { data: 'office' },
+      { data: 'position' },
+      { data: 'start_date' },
+      { data: 'extn' }
+    ]
+  });
+});
